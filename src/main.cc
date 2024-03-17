0d94422c18bda49d3e6462e2ae267ee5e91acbde
ariadnagarcialorente <ariadnag065@gmail.com>
2024-03-17 17:30:01 +0100

    cry2

--- src/main.cc
+++ src/main.cc
@@ -4,13 +4,13 @@ using namespace std;
 
 int main() {
     Kmediods km;
-    string dataset = "Dataset3";
+    string dataset = "Dataset1";
 
 
     km.load_data(dataset + ".csv");
     //for (int k = 2; k <= 10; ++k) {
-        km.execute(2);
-        km.write_results(dataset + "-" + to_string(2) + ".csv");
+        km.execute(7);
+        km.write_results(dataset + "-" + to_string(7) + ".csv");
     //}
     
 }
